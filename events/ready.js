5cf151a4ea786e72b669de5233179eed148f2e81
Kaymaq <67608644+Kaymaq@users.noreply.github.com>
2020-06-30 13:33:15 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,28 +6,10 @@ const ayarlar = require('../ayarlar.json');
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-var oyun = [
-        "HusooTV - ready.js üzerinden değiştir",
-        "satır 1",
-        "satır 2",
-        "satır 3",
-        "satır 4",
-        "satır 5",
-       //DBL EKLENCEKSENİZ 5000 EN İYİ DEĞİŞİM SÜRESİDİR! EKLEMEK İSTEMİYORSANIZ İDEAL 2500
-
-    ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "https://www.youtube.com/channel/UC5mxQbR-4er2giWDTq9SgQg?view_as=subscriber" );
-      }, 2 * 5000); //DEĞİŞME SÜRESİ
-
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yÃ¼klendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriÅŸ yapÄ±ldÄ±!`);
   client.user.setStatus("online");
-  client.user.setActivity(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
-  //console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  //console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  client.user.setGame(` HusooTV /  ready.js Ã¼zerinden deÄŸiÅŸtirebilirsiniz. `);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandÄ±!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Åu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanÄ±cÄ±ya hizmet veriliyor!`);
 };
