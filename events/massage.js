bcaea090a6101589eeee9b6f1bc6c0045cc8fd15
Kaymaq <67608644+Kaymaq@users.noreply.github.com>
2020-06-30 13:32:54 +0300

    Update massage.js

--- events/massage.js
+++ events/massage.js
@@ -1,13 +1,5 @@
 const ayarlar = require('../ayarlar.json');
-let talkedRecently = new Set();
 module.exports = message => {
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  }
-  talkedRecently.add(message.author.id);
-    setTimeout(() => {
-    talkedRecently.delete(message.author.id);
-  }, 2500);
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(ayarlar.prefix)) return;
@@ -15,14 +7,6 @@ module.exports = message => {
   let params = message.content.split(' ').slice(1);
   let perms = client.elevation(message);
   let cmd;
-
-  if (!client.commands.has(command)) {
-    if (client.aliases.has(command)) {
-      return false;
-    } else {
-    }
-  }
-
   if (client.commands.has(command)) {
     cmd = client.commands.get(command);
   } else if (client.aliases.has(command)) {
